24c1660d8e14b508819846dec1b3c0685a92d31d
enpitsulin <enpitsulin@gmail.com>
2026-05-15 16:07:24 +0800

    chore: update bump config

--- bump.config.ts
+++ bump.config.ts
@@ -1,7 +1,13 @@
 import { defineConfig } from 'bumpp'
 
 export default defineConfig({
-  files: ['packages/bulu-cli/package.json', 'packages/tcx-core/package.json', 'packages/tcx-core/Cargo.toml'],
+  files: [
+    'packages/bulu-cli/package.json',
+    'packages/tcx-core/package.json',
+    'packages/tcx-core/Cargo.toml',
+    'packages/tcx-core/index.js',
+    'Cargo.lock',
+  ],
   execute: 'pnpm build',
   commit: true,
   tag: true,
